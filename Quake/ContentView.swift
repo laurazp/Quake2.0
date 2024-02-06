559c8542c1435910c65b609e3fb6bbc2746f8354
laura <69944724+laurazp@users.noreply.github.com>
2024-02-07 00:49:58 +0100

    Project created

---
+++ Quake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quake
+//
+//  Created by Laura Zafra Prat on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quake/QuakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuakeApp.swift
+//  Quake
+//
+//  Created by Laura Zafra Prat on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct QuakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
