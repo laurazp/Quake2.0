3347563a234cb96900007a5d11b172f5ba6b07ef
laura <69944724+laurazp@users.noreply.github.com>
2024-02-12 00:25:03 +0100

    EarthquakesView and EarthquakeItemView updated

---
+++ Quake/Features/Earthquakes/EarthquakesView/EarthquakeItemView.swift
@@ -0,0 +1,65 @@
+//
+//  EarthquakeItemView.swift
+//  Quake
+//
+//  Created by Laura Zafra Prat on 9/2/24.
+//
+
+import SwiftUI
+
+struct EarthquakeItemView: View {
+    let getMagnitudeColorUseCase = GetMagnitudeColorUseCase()
+    let earthquake: Earthquake
+    @State var isExpanded: Bool
+    
+    var body: some View {
+        DisclosureGroup(
+            isExpanded: $isExpanded,
+            content: {
+                ZStack {
+                    NavigationLink(destination: EarthquakeDetailView(earthquake: earthquake)) {
+                        EmptyView()
+                    }.opacity(0.0)
+                    
+                    VStack(alignment: .leading) {
+                        ForEach([
+                            ("Place", earthquake.place),
+                            ("Date", earthquake.date),
+                            ("Tsunami", earthquake.tsunami),
+                            ("Coords", earthquake.formattedCoords),
+                            ("Depth", earthquake.depth)
+                        ], id: \.0) { title, value in
+                            HStack {
+                                Text("\(title):")
+                                    .fontWeight(.bold)
+                                    .foregroundStyle(.gray)
+                                
+                                Text(value)
+                            }
+                        }
+                    }
+                }
+            },
+            label: {
+                HStack (alignment: VerticalAlignment.center, spacing: 12) {
+                    Text(earthquake.magnitude)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(getMagnitudeColorUseCase.getMagnitudeColor(magnitude: Double(earthquake.magnitude)!)))
+                    
+                    Text(earthquake.simplifiedTitle)
+                        .font(.title3)
+                        .foregroundColor(.black)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(6)
+                .multilineTextAlignment(.center)
+            }
+        )
+        .accentColor(.gray)
+    }
+}
+
+#Preview {
+    EarthquakeItemView(earthquake: .example, isExpanded: false)
+}
---
+++ Quake/Features/Earthquakes/EarthquakesView/EarthquakesView.swift
@@ -0,0 +1,51 @@
+//
+//  EarthquakesView.swift
+//  Quake
+//
+//  Created by Laura Zafra Prat on 9/2/24.
+//
+
+import SwiftUI
+
+struct EarthquakesView: View {
+    @StateObject private var viewModel: EarthquakesViewModel
+    @EnvironmentObject var coordinator: Coordinator
+    
+    init(viewModel: EarthquakesViewModel) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+    
+    var body: some View {
+        NavigationStack {
+            if viewModel.isLoading {
+                ProgressView()
+            } else {
+                List(viewModel.earthquakes) { earthquake in
+                    createRow(for: earthquake)
+                        .frame(maxWidth: .infinity)
+                }
+                .navigationTitle("Earthquakes")
+                .listStyle(PlainListStyle())
+            }
+        }
+        .errorLoadingListAlertDialog(error: viewModel.error, errorMessage: viewModel.error?.localizedDescription, retryButtonAction: {
+            Task {
+                await viewModel.getEarthquakes()
+            }
+        })
+        .task {
+            await viewModel.getEarthquakes()
+        }
+    }
+    
+    private func createRow(for earthquake: Earthquake) -> some View {
+        EarthquakeItemView(earthquake: earthquake, isExpanded: false)
+    }
+}
+
+
+#Preview {
+    let coordinator = Coordinator()
+    return coordinator.makeEarthquakesView()
+        .environmentObject(coordinator)
+}
